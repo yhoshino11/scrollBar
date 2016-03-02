3d6ac630c4606960988b4a5c0326c1def0635107
Yu Hoshino <yhoshino11@gmail.com>
2016-03-02 15:29:40 +0900

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('#content').width(400).height(400);
+  $('#content').perfectScrollbar();
+  $('#content').perfectScrollbar('update');
+
+  $('#sub-content').width(400).height(400);
+  $('#sub-content').perfectScrollbar();
+  $('#sub-content').perfectScrollbar('update');
+});
